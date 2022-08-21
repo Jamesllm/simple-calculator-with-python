890ea27fb25f90be97421482ef6388513590f657
Ronal James <92068615+Jamesllm@users.noreply.github.com>
2022-08-20 19:30:51 -0500

    update calculator

--- calculator.py
+++ calculator.py
@@ -1,23 +1,24 @@
-#calculadora basica
+#Simple calculator
 import time
-a = float(input("Ingrese numero 1:  "))
-b = float(input("Ingrese numero 2:  "))
+a = float(input("Enter number 1:  "))
+b = float(input("Enter number 2:  "))
 
-suma = a + b
-resta = a - b
-multiplicacion = a * b
+addition = a + b
+subtraction = a - b
+multiplication = a * b
 division = a / b
 
 print()
 
-print("...Calculando...")
-print("................")
+print("...Calculating...")
+print(".................")
 time.sleep(1.5)
-print("suma" ,round(suma,2))
+print("Addition: " ,round(addition,2))
 time.sleep(1.5)
-print("resta", round(resta,2))
+print("Subtraction: ", round(subtraction,2))
 time.sleep(1.5)
-print("multiplicacion", round(multiplicacion,2))
+print("Multiplication: ", round(multiplication,2))
 time.sleep(1.5)
-print("division", round(division,2))
+print("Division: ", round(division,2))
+time.sleep(1.5)
 print()
